d353161e2496c0e955d77edaad08ccf2a153d9cc
romajs <joseromualdoneto@gmail.com>
2015-09-27 01:21:45 -0300

    uploadFiles & createFolderIfNotExists

--- example.js
+++ example.js
@@ -1,60 +1,50 @@
 #!/usr/bin/env node
 
 /* 
- * Usage: nodejs example.js <client secret path> <parent folder ID> <files to upload>
+ * Usage: nodejs example.js <client secret path> <target folder title> <parent folder ID> <files to upload>
  * 
  * Example: nodejs example.js client_secret.json 0B_SPtvg96z_ZflJSN0x test1.txt test2.txt test3.txt
  */
 
 var googleDriveManager = require('./lib/GoogleDriveManager');
 
-
-var clientSecretJsonPath = process.argv[2]; // params[0] = client secret json path
-var parentFolderId = process.argv[3] // params[1] = files to upload
-var filesToUpload = process.argv.slice(4) // params[2..n] = files to upload
-
-
-console.log('>Client Secret JSON: ' + clientSecretJsonPath);
-console.log('>Parent folder ID: ' + parentFolderId);
-console.log('>Files to upload: ' + filesToUpload);
-
-
+// params[0] = client secret json path
+var clientSecretJsonPath = process.argv[2]; 
+console.log('Client Secret JSON: ' + clientSecretJsonPath);
 if(clientSecretJsonPath == undefined) {
   throw { code: 'MISSING_CLIENT_SECRET', message: 'Client secret NOT specified!' };
 }
+
+// params[1] = target folder title
+var targetFolderTitle = process.argv[3] 
+console.log('Target folder title: ' + targetFolderTitle);
+if(targetFolderTitle == undefined) {
+  throw { code: 'MISSING_TARGET_FOLDER_TITLE', message: 'Target folder title NOT specified!' };
+}
+
+// params[2] = files to upload
+var parentFolderId = process.argv[4] 
+console.log('Parent folder ID: ' + parentFolderId);
 if(parentFolderId == undefined) {
   throw { code: 'MISSING_PARENT_FOLDER_ID', message: 'Parent folder ID NOT specified!' };
 }
+
+// params[3..n] = files to upload
+var filesToUpload = process.argv.slice(5) 
+console.log('Files to upload: ' + filesToUpload);
 if(filesToUpload.length == 0) {
   throw { code: 'MISSING_UPFILES', message: 'Upload files NOT specified!' };
 }
 
-random = function(n) {
-  return Math.round(Math.random() * n);
-}
-
+// our main routine: authorize api, create subfolder and upload files to it
 googleDriveManager.authorize(clientSecretJsonPath).then(function (drive) {
-
-  var folderTitle = 'test-' + random(100);
-  console.info('Initiatin upload to: ', folderTitle);
-
-  return googleDriveManager.searchFolder(folderTitle, parentFolderId).then(function(result) {
-    if(!result.items || result.items.length == 0) {
-      console.log('Folder title not found', folderTitle)
-      return googleDriveManager.createFolder(folderTitle, parentFolderId).then(function(result) {
-        return result.id;
-      });
-    } else {
-      console.log('Retrieving folder title', folderTitle)
-      return result.items[0].id;
-    }
+  console.info('Initializing example file uploading to folder: "' +  targetFolderTitle + '"');
+  return googleDriveManager.createFolderIfNotExists(targetFolderTitle, parentFolderId).then(function(childFolder) {
+    console.info('Uploading files: [', filesToUpload, '] to: ', childFolder.id);
+    return googleDriveManager.uploadFiles(filesToUpload, childFolder.id);
   });
-
-}, function(err) {
-  console.info('Failed', err);
-}).then(function(childFolderId) {
-  console.info('Uploading files: ', filesToUpload, ' to: ', childFolderId);
-  return googleDriveManager.uploadFiles(filesToUpload, childFolderId);
 }).then(function() {
-  console.info('Done');
+  console.info('Done!');
+}, function(err) {
+  console.info('Failed!', err);
 });
